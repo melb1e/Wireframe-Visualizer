83b6265c8cf2b0d671b635a10c78d964fb6776bc
melb1e <84910245+melb1e@users.noreply.github.com>
2022-05-16 17:15:26 +0300

    Update main.c

--- main.c
+++ main.c
@@ -50,39 +50,33 @@ void  matrix_fill(int *m_line, char *line)
 
 void  file_read(t_fdf *matrix)
 {
-  int   i;
-  int   fd;
-  chat  *line;
+  	int   i;
+  	int   fd;
+  	chat  *line;
 
-  i = 0;
-  data->height = get_height(file);
-  data->width = get_width(file);
-  data->matrix = (int **)malloc(sizeof(int *) * (data->height + 1));
-  while (i < data->height)
-    data->matrix[i] = (int *)malloc(sizeof(int) * (data->width + 1));
-  fd = open(file, O_RDONLY, 0);
-  i = 0;
-  while (get_next_line(fd, &line))
-  {
-    fill_matrix(data->matrix[i], line);
-    free(line);
-    i++;
-  }
-  data->matrix[i] = NULL;
+  	i = 0;
+  	data->height = get_height(file);
+  	data->width = get_width(file);
+  	data->matrix = (int **)malloc(sizeof(int *) * (data->height + 1));
+  	while (i < data->height)
+    		data->matrix[i] = (int *)malloc(sizeof(int) * (data->width + 1));
+  	fd = open(file, O_RDONLY, 0);
+  	i = 0;
+  	while (get_next_line(fd, &line))
+  	{
+    		fill_matrix(data->matrix[i], line);
+    		free(line);
+		i++;
+	}
+	data->matrix[i] = NULL;
 }
 
 int		main(int argc, char **argv)
 {
-  t_fdf **matrix;
-
+	t_fdf **matrix;
+	
 	if (argc != 2)
 		ft_error("usage: ./fdf [map name]");
-  matrix = (t_ftf *)malloc(sizeof(fdf));
-  file_read(matrix);
-  
-	matrix = read_map(*++argv);
-	set_default(&PRM);
-	draw(matrix);
-	mlx_key_hook(PRM.win_ptr, deal_key, matrix);
-	mlx_loop(PRM.mlx_ptr);
+ 	matrix = (t_ftf *)malloc(sizeof(fdf));
+ 	file_read(matrix);
 }

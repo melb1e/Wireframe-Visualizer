695b3e16db8e0a0e677df56cbaad81c76dcbbdc0
melb1e <84910245+melb1e@users.noreply.github.com>
2022-05-21 23:35:18 +0300

    Update and rename movements.c to keys.c

--- keys.c
+++ keys.c
@@ -2,6 +2,7 @@
 
 static int	movement_key(int key, t_fdf *matrix)
 {
+	mlx_clear_window(matrix->mlx_ptr, matrix->win_ptr);
 	if (key == 13 || key == 126)
 		matrix->shift_y += 10;
 	else if (key == 0 || key == 123)
@@ -10,20 +11,16 @@ static int	movement_key(int key, t_fdf *matrix)
 		matrix->shift_y -= 10;
 	else if (key == 2 || key == 124)
 		matrix->shift_x += 10;
-	return (matrix);
-}
-
-static int	iso_angle(int key, t_fdf *matrix)
-{
-	if (key == 12)
+	else if (key == 12)
 		matrix->iso_angle -= 10;
-	else
+	else if (key == 14)
 		matrix->iso_angle += 10;
 	return (matrix);
 }
 
 static int	zoom(int key, t_fdf *matrix)
 {
+	mlx_clear_window(matrix->mlx_ptr, matrix->win_ptr);
 	if (key == 15)
 		matrix->zoom += 10;
 	else if (key == 3)
@@ -37,6 +34,7 @@ static int	zoom(int key, t_fdf *matrix)
 
 static int	view(int key, t_fdf *matrix)
 {
+	mlx_clear_window(matrix->mlx_ptr, matrix->win_ptr);
 	if (key == 34)
 		matrix->iso_angle = 0;
 	else if (key == 35)
@@ -54,10 +52,14 @@ static int	view(int key, t_fdf *matrix)
 int	keys(int key, t_fdf *matrix)
 {
 	if (key == 13 || key == 126 || key == 0 || key == 123 || key == 1 || \
-		key == 2 || key == 125 || key == 124)
+		key == 2 || key == 125 || key == 124 || key == 12 || key == 14)
 		matrix = movement_key(key, matrix);
-	else if (key == 12 || key == 14)
-		matrix = iso_angle(key, matrix);
+	else if (key == 53)
+	{
+		mlx_destroy_window(matrix->mlx_ptr, matrix->win_ptr);
+		destruction(matrix);
+		exit(0);
+	}
 	else if (key == 15 || key == 3 || key == 6 || key == 7)
 		matrix = zoom(key, matrix);
 	else if (key == 34 || key == 35 || key == 257)

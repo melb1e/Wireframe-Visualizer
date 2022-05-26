f6a2479c718961f9f01f61fd114164c2f2db5356
Nikita Komissarov <komissarov.na@yandex.ru>
2022-05-26 20:34:08 +0300

    Deleted old files

--- ft_max.c
+++
@@ -1,9 +0,0 @@
-#include "fdf.h"
-
-float	max(float a, float b)
-{
-	if (a < b)
-		return (b);
-	else
-		return (a);
-}
--- ft_mod.c
+++
@@ -1,9 +0,0 @@
-#include "fdf.h"
-
-float	mod(float a, float b)
-{
-	if (n < 0)
-		return (-n);
-	else
-		return (n);
-}
